471e4315a7e77d9a86341a28781446df03361814
Pensologosabo <gabriel.donato@nave.org.br>
2026-07-02 11:05:36 -0300

    Update Atv02SelecionarTodos.js

--- src/Atv02SelecionarTodos.js
+++ src/Atv02SelecionarTodos.js
@@ -14,3 +14,98 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+/*
+  * [ATIVIDADE 1 - Criação do banco]
+  *
+  * Crie um aplicativo Express em uma variável chamada app
+  * que, ao ser inicializado, cria um banco de dados chamado
+  * "beyblade.db". Siga o exemplo no arquivo "app.js" para
+  * referência.
+  *
+  * Após criar o banco de dados, crie uma tabela chamada 
+  * "beyblades" com as colunas abaixo:
+  *
+  * id, com propriedades INTEGER, PRIMARY KEY e AUTOINCREMENT
+  * nome, com propriedades TEXT, NOT NULL e UNIQUE
+  * lamina, com propriedade TEXT,
+  * catraca, com propriedade TEXT,
+  * ponta, com propriedade TEXT,
+  * participante, com propriedades TEXT, NOT NULL e UNIQUE
+  *
+  * Após isso, você deve criar uma rota "GET" no endereço "/"
+  * que envia o arquivo "indexAtv.html".
+  *
+  * Por fim, o servidor deve ouvir a porta 3000.
+  *
+  * Ao final deste arquivo, use "module.exports = app" para
+  * exportar o objeto do servidor para os testes automatizados.
+  */
+
+
+
+const express = require('express')
+const path = require('path')
+const cors = require('cors');
+const sql = require('sqlite3').verbose()
+const porta = 3000
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+const db = new sql.Database(
+  './beyblade.db', 
+  (erro) => {
+    if (erro) {
+      console.error('Erro ao abrir o banco de dados "beyblade.db":', erro.message);
+    } else {
+      console.log('Conectado ao banco de dados SQLite3 "beyblade.db"');
+    }
+  }
+)
+
+db.run(
+  `CREATE TABLE IF NOT EXISTS beyblades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL UNIQUE,
+    lamina TEXT,
+    catraca TEXT,
+    ponta TEXT,
+    participante TEXT NOT NULL UNIQUE
+  )`,
+)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.get('/api/beyblade', (req, res) => {
+
+  // "all" envia todas as informações que combinarem com os parâmetros
+  db.all(
+    // 1º argumento: comando SQL
+    `SELECT * FROM beyblades`, 
+
+    [],
+    // 3º argumento: Função executada após termos o resultado do comando SQL
+    (erro, itensDaTabela) => {
+      // Se der ruim, enviamos a mensagem  de erro
+      if (erro) {
+        res.status(400).json({ error: erro.message })
+        return
+      }
+
+      // Se der bom, enviamos o resultado
+      res.status(200).json({
+        message: "Requisição feita com sucesso",
+        data: itensDaTabela
+      })
+    }
+
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando em http://localhost:${porta}`)
+})
+
