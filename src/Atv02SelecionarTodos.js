25fcc884c4098553f41f1aec8899633f3f85138a
Bruna561 <bruna.nobrega@nave.org.br>
2026-07-02 14:12:18 -0300

    Update Atv02SelecionarTodos.js

--- src/Atv02SelecionarTodos.js
+++ src/Atv02SelecionarTodos.js
@@ -14,3 +14,56 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
+
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const sql = require('sqlite3').verbose()
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'src')))
+
+const db = new sql.Database('./figurinhas.db')
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS beyblades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL UNIQUE,
+    lamina TEXT,
+    catraca TEXT,
+    ponta TEXT,
+    participante TEXT NOT NULL UNIQUE
+  )
+`)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.get('/api/beyblade', (req, res) => {
+  db.all(
+    `SELECT * FROM beyblades`,
+    [],
+    (erro, resultado) => {
+
+      if (erro) {
+        res.status(400).json({ error: erro.message })
+        return
+      }
+
+      res.json({
+        message: "ok",
+        data: resultado
+      })
+    }
+  )
+})
+
+app.listen(3000)
+
+module.exports = app
