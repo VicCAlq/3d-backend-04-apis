cd3ae6c5a3dcb94a7f5e9039ee18b5cb7b2de33d
kevenlwr <keven.santana@nave.org.br>
2026-07-02 10:43:19 -0300

    beybladi
    Create SQLite database and table for beyblades, insert participants.

--- src/Atv01CriacaoBanco.js
+++ src/Atv01CriacaoBanco.js
@@ -24,3 +24,57 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+const  beybladedb = new sql.Database (
+  './beyblade.db',
+  (erro) => { 
+    if (erro) {
+      console.error('Erro ao abrir o banco de dados "beyblade.db":', erro.message);
+    } else {
+      console.log('Conectado ao banco de dados SQLite3 "beyblade.db"');
+    }
+  }
+)
+beybladedb.run (
+  `CREATE TABLE IF NOT EXISTS beyblades
+  
+  id INTEGER PRIMARY KEY AUTOINCREMENT
+  nome TEXT NOT NULL UNIQUE
+  lamina TEXT,
+  catraca TEXT,
+  ponta TEXT,
+  participante TEXT NOT NULL UNIQUE
+   ` 
+)
+
+    db.run(
+        `INSERT INTO  participante (nome, lamina, catraca, ponta, participante ) VALUES
+          (Asa de Valquíria Accel', 'Valtryek Espada', aguda, 'Valt Aoi' ),
+          ('Spriggan Spread Fusion', 'Storm Spryzen', redonda, 'Shuren Kurenai'),
+          ('Drenar Fafnir 8 Nada', 'Garra Geist', agudo e pontudo, 'De La Hoya'),
+        `,
+        (erro) => {
+          if (erro) {
+            console.error('Erro ao criar inserir jogadores na tabela "selecao"', erro.message);
+          } else {
+            console.log('Jogadores inseridos na tabela "selecao');
+          }
+        }
+      )
+
+app.get ( '/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+} );
+
+app.listen(3000, () => {
+    console.log('Servidor rodando! Acesse http://localhost:3000/');
+});
+
+module.exports = app
