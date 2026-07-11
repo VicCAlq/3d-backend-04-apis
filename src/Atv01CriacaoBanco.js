94a55f673432bd8e5357e091c4a1d7414ab74f1c
SenaPaiva <mariaeduarda.sena@nave.org.br>
2026-07-11 18:45:04 -0300

    Update Atv01CriacaoBanco.js

--- src/Atv01CriacaoBanco.js
+++ src/Atv01CriacaoBanco.js
@@ -24,3 +24,55 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+const express = require('express')
+const path = require('path')
+const cors = require('cors');
+const sql = require('sqlite3').verbose()
+
+const porta = 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+const db = new sql.Database(
+‘./beyblade.db’,
+(erro) => {
+    if (erro) {
+      console.error('Erro ao abrir o banco de dados "figurinhas.db":', erro.message);
+    } else {
+      console.log('Conectado ao banco de dados SQLite3 "figurinhas.db"');
+    }
+  }
+)
+
+db.run(
+  ‘CREATE TABLE IF NOT EXIST beyblades (
+   id INTEGER  PRIMARY KEY  AUTOINCREMENT,
+   nome TEXT NOT NULL UNIQUE,
+   lamina TEXT,
+   catraca TEXT,
+   ponta TEXT,
+   participante TEXT NOT NULL UNIQUE
+)’,
+
+(erro) => {
+    if (erro) {
+      console.error('Erro ao criar a tabela "beyblade"', erro.message);
+    } else {
+      console.log('Tabela "beyblade" pronta!');
+
+db.run(
+ ‘INSERT INTO beyblade (nome, caracteristicas, jogador) VALUES
+    )
+  }
+)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+module.exports = app
+
+
