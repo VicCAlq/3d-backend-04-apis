9193450d58c9ef4f96e21a81df45aa741622810f
netod4rk <severino.neto@nave.org.br>
2026-08-10 15:40:10 -0300

    Add comments for module export and database tool

--- src/Atv03AcrescentarItem.js
+++ src/Atv03AcrescentarItem.js
@@ -17,6 +17,7 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
 const express = require('express')
 const path = require('path')
 const cors = require('cors');
