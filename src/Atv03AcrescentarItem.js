15bfd8069df9087c847a77b603d3d5b6a0242d82
netod4rk <severino.neto@nave.org.br>
2026-08-13 11:02:56 -0300

    Refactor Atv03AcrescentarItem.js for clarity and structure

--- src/Atv03AcrescentarItem.js
+++ src/Atv03AcrescentarItem.js
@@ -18,28 +18,16 @@
   * exportar o objeto do servidor para os testes automatizados.
   */
 
-const express = require('express')
-const path = require('path')
-const cors = require('cors');
-// Importando a ferramenta pra utilizar bancos de dados
-const sql = require('sqlite3').verbose()
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
-// Definindo a porta usada para abrir o servidor (no final deste código)
-const porta = 3000
-
-const app = express()
-// Necessário para leitura do corpo de uma requisição
-app.use(express.urlencoded({ extended: true }))
-// Necessário para interpretação do formato JSON
-app.use(express.json())
-// Para evitar erros de CORS
-app.use(cors());
-// Para configurar o servidor para olhar dentro da pasta src por padrão
 app.use(express.static(path.join(__dirname, 'src')));
-
-const db = new sql.Database(
-  './beyblade.db', // Nome do arquivo do banco de dados
-  (erro) => { // Função que executa que o banco é criado
+const beybladedb = require("sqlite3").verbose();
+const  beybladedb = new sql.Database (
+  './beyblade.db',
+  (erro) => { 
     if (erro) {
       console.error('Erro ao abrir o banco de dados "beyblade.db":', erro.message);
     } else {
@@ -47,49 +35,44 @@ const db = new sql.Database(
     }
   }
 )
-db.run(
-  // 1º argumento = comando SQL
-  `CREATE TABLE IF NOT EXISTS beyblades (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  nome TEXT NOT NULL UNIQUE,
+beybladedb.run (
+  `CREATE TABLE IF NOT EXISTS beyblades
+  
+  id INTEGER PRIMARY KEY AUTOINCREMENT
+  nome TEXT NOT NULL UNIQUE
   lamina TEXT,
   catraca TEXT,
   ponta TEXT,
-  participante NOT NULL UNIQUE
-  )`,
-  // 2º argumento = função executada após termos o resultado do comando SQL
-  (erro) => {
-    if (erro) {
-      console.error('Erro ao criar a tabela "beyblades"', erro.message);
-    } else {
-      console.log('Tabela "beyblades" pronta!');
-    }
-  }
+  participante TEXT NOT NULL UNIQUE
+   ` 
 )
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'indexAtv.html'))
-})
 
-app.listen(porta, () => {
-  console.log(`Servidor rodando em http://localhost:${porta}`)
-})
-app.get('/api/beyblade', (req, res) => {
+    db.run(
+        `INSERT INTO  beyblades (nome, lamina, catraca, ponta, participante ) VALUES
+          (Asa de Valquíria Accel', 'Valtryek
+Espada', redondo ,aguda, 'Valt Aoi' ),
+          ('Spriggan Spread Fusion', 'Storm Spryzen', redonda, aguda, 'Shuren Kurenai'),
+          ('Drenar Fafnir 8 Nada', 'Garra Geist', agudo e pontudo, achatado, 'De La Hoya'),
+        `,
+        (erro) => {
+          if (erro) {
+            console.error('Erro ao criar inserir jogadores na tabela "beyblades", erro.message);
+          } else {
+            console.log('Jogadores inseridos na tabela "beyblades");
+          }
+        }
+      )
 
-  // "all" envia todas as informações que combinarem com os parâmetros
-  db.all(
-    // 1º argumento: comando SQL
+      app.get("/api/beyblade", (req, res) => {
+         db.all(
+
     `SELECT * FROM beyblades`, 
-    // 2º argumento: parâmetros (exemplo: posição = "zagueiro")
     [],
-    // 3º argumento: Função executada após termos o resultado do comando SQL
     (erro, itensDaTabela) => {
-      // Se der ruim, enviamos a mensagem  de erro
       if (erro) {
         res.status(400).json({ error: erro.message })
         return
       }
-
-      // Se der bom, enviamos o resultado
       res.status(200).json({
         message: "Requisição feita com sucesso",
         data: itensDaTabela
@@ -97,30 +80,28 @@ app.get('/api/beyblade', (req, res) => {
     }
   )
 })
-app.get('/api/beyblade/cadastrar', (req, res) => {
-  // Se não houver requisição, enviamos um erro
+
+app.get("/api/beyblade/cadastrar", (req, res) => {
+
   if (!req.query) {
     res.status(400).json({ error: erro.message });
     return
   }
 
-  // Extraímos os argumentos enviados pela requisição
   const {
     nome, lamina, catraca, ponta, participante
   } = req.query
 
   db.all(
-    // Comando INSERT no SQL, onde as interrogações vão ser substituídas pelos valores
     `INSERT INTO beyblades (nome, lamina, catraca, ponta, participante) VALUES (?, ?, ?, ?, ?)`,
-    [ nome, lamina, catraca, ponta, participante],
-    // Tratamento básico de erros como nos casos acima
+    [ nome, lamina, catraca, ponta, participante ],
     (erro, itensDaTabela) => {
       if (erro) {
         res.status(400).json({ error: erro.message });
         return;
       }
       res.json({
-        message: `Beyblade ${nome} adicionada com sucesso com lamina ${lamina} catraca ${catraca} ponta ${ponta}`,
+        message: `Jogador ${participante} inscrito com sucesso para o torneio de beyblayd ${nome} com o seu blayd preferido, que tem uma ${catraca} e uma ${lamina} com uma ${ponta}`,
         data: { id: this.lastID },
         id: this.lastID,
         total: itensDaTabela,
@@ -128,3 +109,13 @@ app.get('/api/beyblade/cadastrar', (req, res) => {
     }
   )
 })
+
+app.get ( '/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+} );
+
+app.listen(3000, () => {
+    console.log('Servidor rodando! Acesse http://localhost:3000/');
+});
+
+module.exports = app
