9f30acf4ceba8a2be4b9d6c42085acbfbe41ab99
harieladorecife <mariaantonia.lima@nave.org.br>
2026-09-01 10:16:21 -0300

    Update Atv03AcrescentarItem.js

--- src/Atv03AcrescentarItem.js
+++ src/Atv03AcrescentarItem.js
@@ -18,16 +18,17 @@
   * exportar o objeto do servidor para os testes automatizados.
   */
 
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'src')));
-const beybladedb = require("sqlite3").verbose();
-const  beybladedb = new sql.Database (
+const express = require('express')
+const path = require('path')
+const cors = require('cors');
+const sql = require('sqlite3').verbose();
+const app = express();
+const porta = 3000;
+
+const db = new sql.Database(
   './beyblade.db',
-  (erro) => { 
+  (erro) => {
     if (erro) {
       console.error('Erro ao abrir o banco de dados "beyblade.db":', erro.message);
     } else {
@@ -35,38 +36,36 @@ const  beybladedb = new sql.Database (
     }
   }
 )
-beybladedb.run (
-  `CREATE TABLE IF NOT EXISTS beyblades
-  
-  id INTEGER PRIMARY KEY AUTOINCREMENT
-  nome TEXT NOT NULL UNIQUE
-  lamina TEXT,
-  catraca TEXT,
-  ponta TEXT,
-  participante TEXT NOT NULL UNIQUE
-   ` 
+
+db.run(
+  `CREATE TABLE IF NOT EXISTS beyblades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL UNIQUE,
+    lamina TEXT,
+    catraca TEXT,
+    ponta TEXT,
+    participante TEXT NOT NULL UNIQUE
+  )`,
+ db.run(
+    `INSERT INTO beyblades (nome, lamina, catraca, ponta, participante) VALUES`,
+      (erro) => {
+        if (erro) {
+          console.error('Erro ao criar a tabela "beyblades"', erro.message);
+        } else {
+          console.log('beyblades inseridos na tabela "beyblades');
+        }
+      }
+  )
+)
 )
 
-    db.run(
-        `INSERT INTO  beyblades (nome, lamina, catraca, ponta, participante ) VALUES
-          (Asa de Valquíria Accel', 'Valtryek
-Espada', redondo ,aguda, 'Valt Aoi' ),
-          ('Spriggan Spread Fusion', 'Storm Spryzen', redonda, aguda, 'Shuren Kurenai'),
-          ('Drenar Fafnir 8 Nada', 'Garra Geist', agudo e pontudo, achatado, 'De La Hoya'),
-        `,
-        (erro) => {
-          if (erro) {
-            console.error('Erro ao criar inserir jogadores na tabela "beyblades", erro.message);
-          } else {
-            console.log('Jogadores inseridos na tabela "beyblades");
-          }
-        }
-      )
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
 
-      app.get("/api/beyblade", (req, res) => {
-         db.all(
-
-    `SELECT * FROM beyblades`, 
+app.get('/api/beyblade', (req, res) => {
+  db.all(
+    `SELECT * FROM beyblades`,
     [],
     (erro, itensDaTabela) => {
       if (erro) {
@@ -81,27 +80,25 @@ Espada', redondo ,aguda, 'Valt Aoi' ),
   )
 })
 
-app.get("/api/beyblade/cadastrar", (req, res) => {
-
+app.get('/api/beyblade/cadastrar', (req, res) => {
   if (!req.query) {
     res.status(400).json({ error: erro.message });
     return
   }
-
   const {
     nome, lamina, catraca, ponta, participante
   } = req.query
 
   db.all(
     `INSERT INTO beyblades (nome, lamina, catraca, ponta, participante) VALUES (?, ?, ?, ?, ?)`,
-    [ nome, lamina, catraca, ponta, participante ],
+    [ nome, lamina, catraca, ponta, participante],
     (erro, itensDaTabela) => {
       if (erro) {
         res.status(400).json({ error: erro.message });
         return;
       }
       res.json({
-        message: `Jogador ${participante} inscrito com sucesso para o torneio de beyblayd ${nome} com o seu blayd preferido, que tem uma ${catraca} e uma ${lamina} com uma ${ponta}`,
+        message: `O participante ${nome} com o seu bey beyblade que possui a lamina ${lamina} com a catraca ${catraca} e a ponta ${ponta} ira para o torneio regional!`,
         data: { id: this.lastID },
         id: this.lastID,
         total: itensDaTabela,
@@ -110,12 +107,8 @@ app.get("/api/beyblade/cadastrar", (req, res) => {
   )
 })
 
-app.get ( '/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'indexAtv.html'))
-} );
-
-app.listen(3000, () => {
-    console.log('Servidor rodando! Acesse http://localhost:3000/');
-});
+app.listen(porta, () => {
+  console.log(`Servidor rodando em http://localhost:${porta}`)
+})
 
 module.exports = app
